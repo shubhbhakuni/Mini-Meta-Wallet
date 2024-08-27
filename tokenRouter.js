9e4e1224e2d325d7220a761b49540c8e9651cd47
Shubh Bhakuni <87989282+shubhbhakuni@users.noreply.github.com>
2024-08-27 08:21:05 +0530

    Delete tokenRouter.js

--- tokenRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authController = require("../Controllers/authController");
-
-const router = express.Router();
-
-router.get("/alltoken", authController.allToken);
-router.post("/createtoken", authController.addToken);
-
-module.exports = router;
